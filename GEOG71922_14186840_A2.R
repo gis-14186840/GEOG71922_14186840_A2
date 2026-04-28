157fb15bf3006f7c795381277f19ab897c968a76
gis-14186840 <zhiwei.wu-3@student.manchester.ac.uk>
2026-04-28 22:59:31 +0100

    refer week8 to calculate diversity

--- GEOG71922_14186840_A2.R
+++ GEOG71922_14186840_A2.R
@@ -1,5 +1,11 @@
 # GEOG71922: Assessment2
 # Author: 14186840
+# Topic: Beetles
+
+#load the required libraries
+library(terra)
+library(sf)
+library(vegan)
 
 #load data
 lcm_raster=rast("LCMUK_2000.tif")
@@ -10,5 +16,26 @@ beetle_comm=read.csv("scot_beetle_community.csv",row.names=1)
 beetle_sf=st_as_sf(beetle_env,coords=c("X","Y"),crs=27700)
 
 #extract land-cover values to points
-lcm_extract=terra::extract(LCM, vect(beetle_sf))
+lcm_extract=terra::extract(lcm_raster, vect(beetle_sf))
 beetle_env$Landcover=as.factor(lcm_extract[,2])
+
+
+#calculate richness and shannon
+beetle_env$Richness=rowSums(beetle_comm > 0)
+beetle_env$Shannon=diversity(beetle_comm, index="shannon")
+
+#custom function from week 8 to calculate LCBD
+function_lcbd=function(spe1){
+  ss_mat=spe1
+  ss_mat[]=0
+  for(i in 1:ncol(spe1)){
+    sp.i=spe1[,i]
+    col.i_mean=mean(sp.i)
+    ss_mat[,i]=sapply(sp.i,function(x) (x-col.i_mean)^2)
+  }
+  return(rowSums(ss_mat)/sum(ss_mat))
+}
+beetle_env$LCBD=function_lcbd(beetle_comm)
+
+#test result
+print(table(beetle_env$Landcover,useNA="ifany"))
